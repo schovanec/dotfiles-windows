229bf7a118be550a3c26f63363b29094da571d6f
Steve Chovanec <schovanec@fastmail.fm>
2015-02-04 20:23:01 -0500

    Fix sublime text alias and clean up path maniuplation slightly.

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -1,2 +1,2 @@
 # alias for starting sublime text
-Set-Alias subl (Join-Path ${env:ProgramFiles} "Sublime Text 3\subl.exe")
+Join-Path ${env:ProgramFiles} "Sublime Text 3\subl.exe" | ? { Test-Path $_ } | % { Set-Alias subl $_ }
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,20 +1,20 @@
 # Remember the location of the profile 
 $ProfileRoot = $PSScriptRoot
 
-# Create an alias for launching the Sublime Text Editor
-Set-Alias subl (Join-Path ${env:ProgramFiles} "Sublime Text 3\subl.exe")
-
 # Import modules
 Import-Module posh-git
 
 # Make git output in colour
 Enable-GitColors
 
-# Add git installation to the path to give us access to SSH
-Join-Path ${env:ProgramFiles(x86)} Git\bin | ? { Test-Path $_ } | % { $env:PATH += ";$_" }
+# Add some directories to the path (if they exist)
+@(
+    # Add git installation to the path to give us access to SSH
+    (Join-Path ${env:ProgramFiles(x86)} Git\bin),
 
-# Add our custom scripts directory to the path
-Join-Path $ProfileRoot scripts | ? { Test-Path $_ } | % { $env:PATH += ";$_" }
+    # Add our custom scripts directory to the path
+    (Join-Path $ProfileRoot scripts)
+) | ? { Test-Path $_ } | % { $env:PATH += ";$_" }
 
 # Include other scripts
 @(
