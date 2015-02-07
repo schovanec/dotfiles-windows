851ee1cc1dadd48b34a853a2fdc8f86b95d95806
Steve Chovanec <schovanec@fastmail.fm>
2015-02-06 19:03:02 -0500

    Add script for capturing environment from a batch file

---
+++ powershell/scripts/Invoke-BatchFile.ps1
@@ -0,0 +1,14 @@
+[CmdletBinding()]
+Param (
+    [Parameter(Position = 0, Mandatory = $true)]
+    [string]$File, 
+
+    [Parameter(Position = 1, ValueFromRemainingArguments = $true)]
+    $Parameters
+)
+
+cmd /c "`"$File`" $Parameters && set" | ForEach {
+    If ($_ -match "^(.*?)=(.*)$") {
+        Set-Content "Env:$($matches[1])" $matches[2]
+    }
+}
