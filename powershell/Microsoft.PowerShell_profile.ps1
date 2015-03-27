bd411535cc8e31260480bfac2e1d07af87719f3e
Steve Chovanec <git@stevechovanec.com>
2015-03-27 17:01:27 -0400

    Move things which interfere with package manager console out of profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,11 @@
 # This file is only executed by the full powershell console host (i.e. not visual studio, etc.)
 
+# Import modules
+Import-Module posh-git
+
+# Make git output in colour
+Enable-GitColors
+
 # Execute newest visual studio command prompt batch file and 
 Get-ChildItem Env: `
     | ? { $_.Name -match "VS\d+COMNTOOLS" } `
@@ -8,3 +14,10 @@ Get-ChildItem Env: `
     | ? { Test-Path $_ } `
     | Select-Object -First 1 `
     | % { Invoke-BatchFile $_ $(if ([Environment]::Is64BitProcess) {'amd64'} else {'x86'}) }
+	
+# Include other scripts
+@(
+    "aliases.ps1",
+    "prompt.ps1",
+    "local.ps1"
+) | % { Join-Path $ProfileRoot $_ } | ? { Test-Path $_ } | % { . $_ }
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,11 +1,6 @@
 # Remember the location of the profile 
 $ProfileRoot = $PSScriptRoot
 
-# Import modules
-Import-Module posh-git
-
-# Make git output in colour
-Enable-GitColors
 
 # Add some directories to the path (if they exist)
 @(
@@ -15,10 +10,3 @@ Enable-GitColors
     # Add our custom scripts directory to the path
     (Join-Path $ProfileRoot scripts)
 ) | ? { Test-Path $_ } | % { $env:PATH += ";$_" }
-
-# Include other scripts
-@(
-    "aliases.ps1",
-    "prompt.ps1",
-    "local.ps1"
-) | % { Join-Path $ProfileRoot $_ } | ? { Test-Path $_ } | % { . $_ }
