7866dd5ed61d00866ac653f0f07ad8b436e03741
Steve Chovanec <git@stevechovanec.com>
2016-04-03 02:27:50 -0400

    Remove sublime text config

--- bootstrap/Install-DotFiles.ps1
+++ bootstrap/Install-DotFiles.ps1
@@ -7,8 +7,5 @@ Import-Module (Join-Path $dotfilesRoot powershell\modules\mklink)
 # link powershell profile directory
 New-SymLink (Join-Path $documentsPath WindowsPowerShell) (Join-Path $dotfilesRoot powershell) -Directory
 
-# link sublime text directory
-New-SymLink (Join-Path ${env:APPDATA} "Sublime Text 3") (Join-Path $dotfilesRoot sublime-text) -Directory
-
 # link git config files
 Get-ChildItem (Join-Path $dotfilesRoot git) -Filter .* | % { New-SymLink (Join-Path $gitHome $_.Name) $_.FullName -File }
--- bootstrap/Remove-DotFiles.ps1
+++ bootstrap/Remove-DotFiles.ps1
@@ -7,9 +7,6 @@ Import-Module (Join-Path $dotfilesRoot powershell\modules\mklink)
     # powershell profile
     (Join-Path $documentsPath WindowsPowerShell),
 
-    # sublime text configuration
-    (Join-Path ${env:APPDATA} "Sublime Text 3"),
-
     # git configuration files
     (Get-ChildItem (Join-Path $dotfilesRoot git) -Filter .* | % { Join-Path ~ $_.Name })
 
